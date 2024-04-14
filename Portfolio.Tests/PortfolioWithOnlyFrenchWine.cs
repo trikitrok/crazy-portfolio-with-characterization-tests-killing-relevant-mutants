8958eadf22b3d0226848661aa2089d3f340baa99
trikitrok <trikitrok@gmail.com>
2024-04-15 01:21:52 +0200

    adding tests that kill relevant mutant signaling missing boundary condition testing

--- Portfolio.Tests/PortfolioWithOnlyFrenchWine.cs
+++ Portfolio.Tests/PortfolioWithOnlyFrenchWine.cs
@@ -6,17 +6,32 @@ namespace Portfolio.Tests;
 
 public class PortfolioWithOnlyFrenchWine
 {
-    [Test]
-    public void value_grows_by_20_before_now()
+    [TestCase(100)]
+    [TestCase(199)] // off point for asset value boundary between (-inf, 200) y [200, +inf] when asset date before now
+    public void when_value_is_less_than_200_it_grows_by_20_before_now(int assetValue)
     {
         var portfolio = APortFolio()
-            .With(AnAsset().DescribedAs("French Wine").FromDate("2024/01/15").WithValue("100"))
+            .With(AnAsset().DescribedAs("French Wine").FromDate("2024/01/15").WithValue(assetValue))
             .OnDate("2025/01/01")
             .Build();
 
         portfolio.ComputePortfolioValue();
 
-        Assert.That(portfolio._messages[0], Is.EqualTo("120"));
+        Assert.That(portfolio._messages[0], Is.EqualTo($"{assetValue + 20}"));
+    }
+    
+    [TestCase(200)] // on point for asset value boundary between (-inf, 200) y [200, +inf] when asset date before now
+    [TestCase(201)] 
+    public void when_value_is_equal_or_greater_than_200_it_remains_the_same_before_now(int assetValue)
+    {
+        var portfolio = APortFolio()
+            .With(AnAsset().DescribedAs("French Wine").FromDate("2024/01/15").WithValue(assetValue))
+            .OnDate("2025/01/01")
+            .Build();
+
+        portfolio.ComputePortfolioValue();
+
+        Assert.That(portfolio._messages[0], Is.EqualTo($"{assetValue}"));
     }
 
     [Test]
